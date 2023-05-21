75cfacac6b2eb847ff970c8eb46b5f0600978b32
psyfry <psyfry@protonmail.com>
2023-05-21 14:54:30 -0700

    fix jest tests

--- utils/config.js
+++ utils/config.js
@@ -5,7 +5,7 @@ const MONGO_URI =
         ? process.env.TEST_MONGO_URI
         : process.env.MONGO_URI
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT | 3003
 module.exports = {
     MONGO_URI,
     PORT
