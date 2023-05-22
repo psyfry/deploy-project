8b0ad5a4adabbec813d413077e4896b1d2a043a6
PG <psyfry@protonmail.com>
2023-05-22 12:05:41 -0700

    E2E tests passing

--- cypress/e2e/homepage.cy.js
+++ cypress/e2e/homepage.cy.js
@@ -1,11 +1,11 @@
 // const cy = require('cypress')
 describe('Hompage shows', () => {
   it('shows navbar logo text', () => {
-    cy.visit('localhost:3030')
-    cy.contains('crowdref')
+    cy.visit('http://localhost:3030/')
+    cy.contains('CROWDREF')
   })
   it('shows login Button', () => {
-    cy.visit('localhost:3030')
-    cy.contains('login')
+    cy.visit('http://localhost:3030')
+    cy.contains('Login')
   })
 })
